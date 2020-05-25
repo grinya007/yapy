785b6c41c6c8d5555ea8f7d132c7fee450f12a79
Gregory Arefyev <gregory@recom.live>
2020-05-25 23:25:38 +0200

    Setting up nginx

---
+++ fastapi/app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException, WebSocket
+from pydantic import BaseModel
+
+app = FastAPI()
+
+items = {}
+
+class Item(BaseModel):
+    id: int
+    name: str
+
+@app.get("/api/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/api/items/{item_id}")
+def read_item(item_id: int):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"item": items[item_id]}
+
+@app.post("/api/items/")
+async def create_item(item: Item):
+    items[item.id] = item
+    return item
+
